f2cc7db9013c4b70ea8a7f1ca82bc7f8d50190b9
Suhas Sumukh <70210383+suhasasumukh@users.noreply.github.com>
2024-01-02 19:23:32 +0530

    Langchain - LLM New updated file

---
+++ index.js
@@ -0,0 +1,18 @@
+import generateResponse from "./lib/generateResponse.js";
+import promptSync from 'prompt-sync';
+
+const prompt = promptSync();
+
+const conversationHistory = [];
+
+while (true) {
+  const question = prompt("Ask a question >");
+  const answer = await generateResponse({
+    prompt: question,
+    history: conversationHistory
+  });
+
+  console.log(`Amjad Masad: ${answer}\n`);
+  
+  conversationHistory.push(`Human: ${question}`, `Amjad Masad: ${answer}`)
+}
