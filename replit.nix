ddc9fd763ff77597ca404f157ae07250edc83065
Suhas Sumukh <70210383+suhasasumukh@users.noreply.github.com>
2024-01-27 23:06:40 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -11,12 +11,12 @@
 	];
   env = {
     PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
+      # Needed for running pandas / numpy
       pkgs.stdenv.cc.cc.lib
       pkgs.zlib
-      # Needed for pygame
+      # Needed for running pygame
       pkgs.glib
-      # Needed for matplotlib
+      # Needed for running matplotlib
       pkgs.xorg.libX11
     ];
     PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
